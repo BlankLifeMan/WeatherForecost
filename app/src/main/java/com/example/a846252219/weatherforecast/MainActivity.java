49a73b867be7e62b4020b706f7a99ccfecb85d13
BlankLifeMan <1430495506@qq.com>
2018-10-05 14:14:39 +0800

    第一次提交

---
+++ app/src/main/java/com/example/a846252219/weatherforecast/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a846252219.weatherforecast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
